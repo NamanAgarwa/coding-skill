16df4948cbd95ef6905f8443980022f6c320a2fc
NamanAgarwa <agarwalnaman946@gmail.com>
2025-07-14 10:36:22 +0530

    max subarray

--- MaxSubArray.js
+++ MaxSubArray.js
@@ -9,3 +9,39 @@
 // Input: nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 // Output: 6
 // Explanation: [4, -1, 2, 1] has the largest sum = 6
+
+// function maxSubArrayWithElements(nums) {
+//   let currentSum = nums[0];
+//   let maxSum = nums[0];
+
+//   for (let i = 0; i < nums.length; i++) {
+//     currentSum = Math.max(nums[i], currentSum + nums[i]);
+//     maxSum = Math.max(currentSum, maxSum);
+//   }
+//   return maxSum;
+// }
+
+function maxSubArrayWithElements(nums) {
+  let currentSum = nums[0];
+  let maxSum = nums[0];
+  let start = 0;
+  let end = 0;
+  let temp = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > currentSum + nums[i]) {
+      currentSum = nums[i];
+      temp = i;
+    } else {
+      currentSum = currentSum + nums[i];
+    }
+
+    if (currentSum > maxSum) {
+      maxSum = currentSum;
+      start = temp;
+      end = i;
+    }
+  }
+  return maxSum;
+}
+
+console.log(maxSubArrayWithElements([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
